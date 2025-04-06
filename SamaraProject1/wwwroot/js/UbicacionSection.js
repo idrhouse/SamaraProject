08ab9c696ef566c9e757a71001cbe963eefd522d
gabogj03 <gabogonzarez@gmail.com>
2025-04-05 23:30:06 -0600

    mejorado

--- SamaraProject1/wwwroot/js/UbicacionSection.js
+++ SamaraProject1/wwwroot/js/UbicacionSection.js
@@ -150,7 +150,7 @@
                     target: "_blank",
                     rel: "noopener noreferrer",
                     className: "inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                }, "Ver en Google Maps"))
+                }, "Ver en Google"))
             ])
         ]))
     ]));
